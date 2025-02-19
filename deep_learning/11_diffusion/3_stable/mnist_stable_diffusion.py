b30129410cb9ff00f74dbfeef4236766a1e11ae7
wangrl2016 <wangrl2016@gmail.com>
2025-02-19 19:51:04 +0800

    Update content

--- deep_learning/11_diffusion/3_stable/mnist_stable_diffusion.py
+++ deep_learning/11_diffusion/3_stable/mnist_stable_diffusion.py
@@ -7,6 +7,29 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 import torch
 
+# residual block
+class ResBlock(nn.Module):
+    def __init__(self, in_channel, time_emb_dim, out_channel=None,):
+        super().__init__()
+        if out_channel is None:
+            out_channel = in_channel
+        self.norm1 = nn.GroupNorm(32, in_channel, eps=1e-05, affine=True)
+
+class UNet(nn.Module):
+    '''
+    A time-dependent score-based model built upon U-Net architecture.
+    '''
+    def __init__(self, marginal_prob_std, channels=[32, 64, 128, 256], embed_dim=256):
+        '''
+        '''
+        super().__init__()
+
+    def forward(self, x, t, y=None):
+        # Obtain the Gaussian random feature embedding for t.
+        embed = self.act(self.time_embed(t))
+        
+
+
 num_steps = 500
 def Euler_Maruyama_sampler(score_model,
                            marginal_prob_std,
