46106a9853b029b46f3b8d38fc89588dc9f57201
wangrl2016 <wangrl2016@gmail.com>
2025-02-21 09:57:56 +0800

    Selection sort

--- data_algorithm/01_array_list/2_linked_list/singly_linked_list.py
+++ data_algorithm/01_array_list/2_linked_list/singly_linked_list.py
@@ -3,6 +3,7 @@ class SinglyListNode:
         self.val: int = val
         self.next: SinglyListNode | None = None
 
+
 n0 = SinglyListNode(1)
 n1 = SinglyListNode(2)
 n2 = SinglyListNode(3)
@@ -14,19 +15,23 @@ n1.next = n2
 n2.next = n3
 n3.next = n4
 
+
 def print_list(iter_node):
     while iter_node != None:
-        print(iter_node.val, end=' ')
+        print(iter_node.val, end=" ")
         iter_node = iter_node.next
     print()
 
+
 print_list(n0)
+
 
 def insert(node: SinglyListNode, inserted: SinglyListNode):
     # 在链表的节点 node 之后插入节点 inserted
     temp = node.next
     node.next = inserted
     inserted.next = temp
+
 
 inserted = SinglyListNode(6)
 insert(n2, inserted)
@@ -38,8 +43,10 @@ def remove(node: SinglyListNode):
         return
     node.next = node.next.next
 
+
 remove(n2)
 print_list(n0)
+
 
 def access(head: SinglyListNode | None, index: int) -> SinglyListNode | None:
     for _ in range(index):
@@ -48,9 +55,11 @@ def access(head: SinglyListNode | None, index: int) -> SinglyListNode | None:
         head = head.next
     return head
 
+
 n3 = access(n0, 3)
 if n3:
     print(n3.val)
+
 
 def find(head: SinglyListNode | None, target: int) -> int:
     # 在链表中查找值为 target 的首个节点
@@ -61,5 +70,6 @@ def find(head: SinglyListNode | None, target: int) -> int:
         head = head.next
         index += 1
     return -1
+
 
 print(find(n0, 2))
--- data_algorithm/04_sort/1_select/selection_sort.py
+++ data_algorithm/04_sort/1_select/selection_sort.py
@@ -1,15 +1,15 @@
-
 def selection_sort(nums: list[int]):
     n = len(nums)
-    # 外循环：未排序区间未 [i, n-1]
+    # 外循环：未排序区间为 [i, n-1]
     for i in range(n - 1):
         # 内循环：找到未排序区间内的最小元素
         k = i
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             if nums[j] < nums[k]:
                 k = j
         # 将该最小元素与未排序区间的首个元素交换
         nums[i], nums[k] = nums[k], nums[i]
     return nums
 
-print(selection_sort([4, 7, 5, 1, 8, 3, 9, 2]))
+
+print(selection_sort([6, 4, 7, 5, 1, 8, 3, 9, 2]))
