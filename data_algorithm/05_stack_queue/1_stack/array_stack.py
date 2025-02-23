543f3d7679d1139571e4942507a07c5abf3422b4
wangrl2016 <wangrl2016@gmail.com>
2025-02-23 21:24:50 +0800

    Implement queue using stacks

--- data_algorithm/05_stack_queue/1_stack/array_stack.py
+++ data_algorithm/05_stack_queue/1_stack/array_stack.py
@@ -2,26 +2,25 @@ class ArrayStack:
     '''
     基于数组实现的栈
     '''
-
     def __init__(self):
-        self._stack = []
+        self._stack: list[int] = []
 
     def size(self):
         return len(self._stack)
 
-    def is_empty(self):
-        return self.size == 0
+    def empty(self):
+        return self.size() == 0
 
     def push(self, item):
         self._stack.append(item)
 
     def pop(self):
-        if self.is_empty():
+        if self.empty():
             raise IndexError('Index error')
         return self._stack.pop()
 
     def peek(self):
-        if self.is_empty():
+        if self.empty():
             raise IndexError('Stack is empty')
         return self._stack[-1]
 
