cfd60241d027895ac1c00f2fe344f92d21f471ca
wangrl2016 <wangrl2016@gmail.com>
2025-04-01 18:29:40 +0800

    Param change

--- pytorch/01_tensor/3_principle/fit_line.py
+++ pytorch/01_tensor/3_principle/fit_line.py
@@ -24,6 +24,13 @@ for _ in range(epochs):
             param -= step
         intermediate_list.append(param)
 print('Last param is', round(intermediate_list[-1], 4))
+
+pyplot.scatter(input, y_true)
+pyplot.grid(True)
+pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
+pyplot.plot(input, intermediate_list[-1] * input + 4, color='red')
+pyplot.show()
+
 pyplot.plot(range(len(intermediate_list)), intermediate_list)
 pyplot.grid(True)
 pyplot.subplots_adjust(left=0.08, right=0.92, top=0.96, bottom=0.06)
