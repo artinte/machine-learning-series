a8a1e703343dba1e04206e126d4b9ea676327d7a
wangrl2016 <wangrl2016@gmail.com>
2025-04-12 22:29:04 +0800

    Tensor operate

--- pytorch/01_tensor/2_tensor/tensor_operate.py
+++ pytorch/01_tensor/2_tensor/tensor_operate.py
@@ -31,5 +31,45 @@ assert b.shape == (3, 1)
 assert c.shape == (3, 3)
 print(c)
 
-tensor = torch.tensor([1, 2, 3, 4, 5, 6])
-reshaped_tensor = tensor.view(2, 3)
+x = torch.randn((2, 3, 4))
+y = x.view((6, 4))
+assert x.shape == (2, 3, 4)
+assert y.shape == (6, 4)
+
+z = x.view(-1, 4)
+assert z.shape == (6, 4)
+
+x = torch.randn((2, 3, 4))
+y = x.reshape((6, 4))
+assert x.data_ptr() == y.data_ptr()
+
+x = torch.randn((2, 3))
+y = x.transpose(0, 1)
+assert y.shape == (3, 2)
+
+x = torch.randn(2, 3, 4)
+y = x.transpose(1, 2)
+assert y.shape == (2, 4, 3)
+
+x = torch.randn((1, 3, 1, 4))
+y = x.squeeze()
+assert y.shape == (3, 4)
+
+x = torch.randn((1, 3, 1, 4))
+y = x.squeeze(0)
+assert y.shape == (3, 1, 4)
+
+x = torch.randn((3, 4))
+y = x.unsqueeze(0)
+assert y.shape == (1, 3, 4)
+
+x = torch.randn((2, 3, 4))
+y = x.permute(2, 0, 1)
+assert y.shape == (4, 2, 3)
+
+x = torch.randn((2, 3, 4))
+y = x.flatten()
+assert y.shape == (24,)
+
+y = x.flatten(start_dim=1)
+assert y.shape == (2, 12)
